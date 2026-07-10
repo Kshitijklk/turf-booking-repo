cdccd21d1a2021d590a8c4830eff71d8f4d741ed
Kshitijklk <kshitijsklk@gmail.com>
2026-07-10 11:27:26 +0530

    insertMany prediction

--- src/crudlab.js
+++ src/crudlab.js
@@ -6,14 +6,19 @@ async function run() {
     const db = mongoose.connection.db;
 
 
-    // ---- BLOCK 5: index implementation ---- 
-    // PREDICTION:It will return an object with name 1
-    const r5 = await db.collection('lab').createIndex(
-        { name: 1 },
-        { unique: true }
-    );
-    console.log('createIndex:', r5);
-    // ACTUAL:createIndex: name_1
+    // ---- BLOCK 6: insertMany with one violation ---- 
+    // PREDICTION: it will insert the first 2 documents, ignore the document that violates the unique
+    // constraint, and then insert the last 2 documents. It will return an object with the number of inserted documents and the ids of the inserted documents.
+    const r5 = await db.collection('lab').insertMany([]
+        { name: "maddie", n: 3 },
+        { name: "travis", n: 4 },
+        { name: "travis", n: 5 },
+        { name: "rick", n: 6 },
+        { name: "carl", n: 7 }
+    
+    ]);
+    console.log('insertMany:', r5);
+    // ACTUAL:
 
 
 
