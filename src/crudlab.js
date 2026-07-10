d7b0fd9603127cbc1246fa37302b8314e8424ff2
Kshitijklk <kshitijsklk@gmail.com>
2026-07-10 12:23:06 +0530

    actual

--- src/crudlab.js
+++ src/crudlab.js
@@ -9,14 +9,14 @@ async function run() {
     // ---- BLOCK 9: updateOne without $set ---- 
     // PREDICTION: it will update the document with name "justin" and set the field n to 10.
     const r8 = await db.collection('lab').updateOne(
-        { name: "justin" }, {{ n: 10 } }
+        { name: "justin" }, { n: 10 }
     );
 
 
-console.log('updateOne:', r8);
-// ACTUAL: 
-await mongoose.disconnect();
+    console.log('updateOne:', r8);
+    // ACTUAL: it threw an error: "MongoServerError: The update operation document must contain atomic operators."  
+    await mongoose.disconnect();
+
+
 }
-
-
 run();
